dbaeaf4d50724c920f56f94ea7178cd45904ad10
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-05-03 02:35:24 +0530

    Fix Product Slider Section fro md sm and lg screen sizes

--- src/components/ProductSection.tsx
+++ src/components/ProductSection.tsx
@@ -3,20 +3,28 @@ import ProductCard from "./ProductCard";
 export default function ProductSlider() {
     return (
         <div className="container my-40">
-            <div className="hidden md:flex md:flex-1/4 justify-between">
-            
-                {/* Desktop View: Fixed 4 cards */}
+            {/* Desktop View: Fixed 4 cards */}
+            <div className="hidden lg:flex justify-evenly">
                 {Array.from({ length: 4 }).map((_, index) => (
-                    <div key={index} className="">
+                    <div key={index} className="w-[23%]">
                         <ProductCard />
                     </div>
                 ))}
             </div>
 
-            <div className="flex md:hidden container scrollbar-hide">
-                {/* Mobile View: Sliding */}
+            {/* Medium and Small screens: Scrollable */}
+            <div className="flex lg:hidden overflow-x-auto scrollbar-hide space-x-4">
                 {Array.from({ length: 8 }).map((_, index) => (
-                    <div key={index} className="flex-shrink-0 w-[70%] h-full">
+                    <div
+                        key={index}
+                        className="
+                            flex-shrink-0 
+                            w-[70%] 
+                            sm:w-[45%] 
+                            md:w-[45%] 
+                            h-full
+                        "
+                    >
                         <ProductCard />
                     </div>
                 ))}
