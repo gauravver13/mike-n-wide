e0a60c55a55ebdf88ff6c28a178b30dfe1b60d3a
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2025-05-04 20:52:47 +0530

    Fix mobile screen Banner

--- src/components/ui/Banner.tsx
+++ src/components/ui/Banner.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 export default function Banner1() {
     return (
-        <div className="mb-0 container">
+        <div className="mb-0 w-full">
             <section className="relative w-full h-screen flex flex-col md:flex-row">
                 {/* Left Image - Full width on mobile */}
                 <div className="relative w-full md:w-1/2 h-full">
@@ -29,9 +29,9 @@ export default function Banner1() {
             </section>
 
             {/* Text and Button Section */}
-            <div className="relative w-full flex flex-col items-start md:items-center justify-center px-4 -mt-64 md:-mt-52 lg:-mt-24 z-10">
+            <div className="relative text-white w-full flex flex-col items-start md:items-center justify-center px-4 -mt-[340px] sm:font-bold md:-mt-52 lg:-mt-32 z-10">
 
-                <div className="text-left lg:text-center leading-normal flex flex-wrap sm:-mt-12 sm:font-bold text-xl lg:text-4xl font-semibold tracking-normal">
+                <div className="text-left lg:text-center leading-normal flex flex-wrap text-xl lg:text-4xl font-semibold tracking-normal">
                     BLEND OF ITALIAN & MODERN ROCO
                 </div>
                 <div className="mt-2">
